1eb37882b4f9de271d6b0f92388bc6d706580dfa
Mikael Roos <mikael.t.h.roos@gmail.com>
2022-04-21 23:28:30 +0200

    Minor edit

--- gui.bash
+++ gui.bash
@@ -2,7 +2,7 @@
 #
 # GUI for dbwebb inspect.
 #
-VERSION="v2.10.1 (2022-04-21)"
+VERSION="v2.10.2 (2022-04-21)"
 
 # Messages
 MSG_OK="\033[0;30;42mOK\033[0m"
@@ -1138,7 +1138,7 @@ downloadParts()
     do
        printf "$part...";
        dbwebb --force --yes --silent download "$part" "$ACRONYM" > /dev/null
-       if (( $? != 0 )); then
+       if (( $? )); then
            return 1
        fi
    done < "$file"
@@ -1179,6 +1179,7 @@ downloadPotato()
         fi
     fi
 
+    printf "me..."
     if ! dbwebb --force --yes download me $ACRONYM > /dev/null; then
         printf "\n\033[32;01m---> Doing a Potato\033[0m\n\033[0;30;43mACTION NEEDED...\033[0m\n" | tee -a "$LOGFILE"
         potatoe $ACRONYM
